67baf1b5f37dd003c890cda48f0ec332720ec043
rexjoseph <josephrex96@gmail.com>
2023-03-06 18:49:08 +0100

    add defaults to models

--- controllers/Index.js
+++ controllers/Index.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 // GET
 exports.getAllUserCount = async (req, res, next) => {
   try {
-    const allUsers = User.find().sort({ createdAt:-1 });
+    const allUsers = await User.find().sort({ createdAt:-1 });
     res.status(200).json(allUsers);
   } catch (err) {
     res.status(500).json(err);
--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  profile_photo: { type: String, default: 'https://a0.muscache.com/defaults/user_pic-225x225.png?v=3' }
 }, { timestamps: true })
 
 module.exports = mongoose.model("User", UserSchema);
