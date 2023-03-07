0b14740428d28070a9d1cf6e36b88757c8ef656b
rexjoseph <josephrex96@gmail.com>
2023-03-07 04:56:20 +0100

    setup package.json and index.js for first heroku deploy

--- index.js
+++ index.js
@@ -9,8 +9,28 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(compression());
+
 const authRoute = require('./routes/auth');
 const indexRoute = require('./routes/index');
+
+// redirect to secure https:
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  } else {
+    next();
+  }
+});
+
+// redirect to main domain
+// app.use((req, res, next) => {
+//   if (req.hostname == 'cohomy.herokuapp.com') {
+//     res.redirect(`https://cohomy.com${req.url}`)
+//   } else {
+//     next();
+//   }
+// });
+
 app.use('/api/auth', authRoute);
 app.use('/api/index', indexRoute);
 
@@ -22,6 +42,13 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => {
   console.log(err);
 })
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Cohomy server running` );
